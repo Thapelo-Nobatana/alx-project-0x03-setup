910606ea4498c59ca7e5f01c436348202726a045
Thapelo Nobatana <nobatanathapelo1@gmail.com>
2025-08-04 19:43:31 +0200

    feat: set up landing page with imperative routing using useRouter

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/router";
 
 
 export default function Home() {
-  const router = useRouter()
+  const router = useRouter();
 
   // Imeperative routing with useRouter
   const routeToNextPage  = ({ pageRoute }: PageRouteProps) => {
